5c4a7019b5fbec5c9f71ff3524044d329a0557fc
Vitrolo <viniciusrabelodominguesdasilva@gmail.com>
2024-10-29 16:47:21 -0300

    So terminei o Post

--- AtividadeComAPI/backend/server.js
+++ AtividadeComAPI/backend/server.js
@@ -3,6 +3,11 @@ const db = require('./db');
 const app = express();
 app.use(express.json());
 const port = 3000;
+const cors = require('cors');
+
+app.use(cors({
+    origin: "*"
+}));
 
 let veiculos = [];
 
--- AtividadeComAPI/frontend/src/App.jsx
+++ AtividadeComAPI/frontend/src/App.jsx
@@ -1,44 +1,59 @@
-import { useEffect, useState } from 'react'
-import './App.css'
+import { useState, useEffect } from 'react'
 import axios from 'axios'
+import './App.css'
 
 function App() {
-  const [marca, setMarca] = useState("")
-  const [modelo, setModelo] = useState("")
+  const [marca, setMarca] = useState('')
+  const [modelo, setModelo] = useState('')
   const [ano, setAno] = useState(0)
-  const [proprietario, setProprietario] = useState("")
-  const [cor, setCor] = useState("")
+  const [proprietario, setProprietario] = useState('')
+  const [cor, setCor] = useState('')
 
   useEffect(() => {
     console.log(marca, modelo, ano, cor, proprietario)
   }, [marca, modelo, ano, cor, proprietario])
 
-  async function registrar() {
+  async function registerVehicle() {
     await axios.post("http://localhost:3000/inserir", {
-      marca,
-      modelo,
-      ano,
-      cor,
-      proprietario
+      marca, modelo, ano, cor, proprietario
     })
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault()
+    registerVehicle()
   }
 
   return (
     <>
-      <div className='formulario'>
-        <form onSubmit={registrar}>
+      <div className="inserir">
+        <form onSubmit={handleSubmit}>
           <label htmlFor="marca">Marca</label>
+          <br />
           <input type="text" id='marca' onChange={(e) => { setMarca(e.target.value) }} />
+          <br />
           <label htmlFor="modelo">Modelo</label>
+          <br />
           <input type="text" id='modelo' onChange={(e) => { setModelo(e.target.value) }} />
+          <br />
           <label htmlFor="ano">Ano</label>
-          <input type="number" id='ano' onChange={(e) => { setAno(Number(e.target.value)) }} />
-          <label htmlFor="proprietario">Proprietario</label>
-          <input type="text" id='proprietario' onChange={(e) => { setProprietario(e.target.value) }} />
+          <br />
+          <input type="text" id='ano' onChange={(e) => { setAno(Number(e.target.value)) }} />
+          <br />
           <label htmlFor="cor">Cor</label>
+          <br />
           <input type="text" id='cor' onChange={(e) => { setCor(e.target.value) }} />
-          <button type='submit' onClick={registrar}>Registrar</button>
+          <br />
+          <label htmlFor="proprietario">Proprietário</label>
+          <br />
+          <input type="text" id='proprietario' onChange={(e) => { setProprietario(e.target.value) }} />
+          <br />
+          <br />
+          <button type='submit' onClick={handleSubmit}>Registrar veículo</button>
         </form>
+      </div>
+      <div className='ver'>
+          <button type='submit' >Select</button>
       </div>
     </>
   )
